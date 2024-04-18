03299c0b24c09cc8829be786698941c373e25f93
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-04-18 12:29:26 +0200

    Add basename to router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,13 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import TaskPage from "./pages/TaskPage";
 import ProjectPage from "./pages/ProjectPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <ProjectPage />,
+        },
+        {
+            path: "/project/:id",
+            element: <TaskPage />,
+        },
+    ],
     {
-        path: "/",
-        element: <ProjectPage />,
-    },
-    {
-        path: "/project/:id",
-        element: <TaskPage />,
-    },
-]);
+        basename: "/Strony/project-dashboard/frontend",
+    }
+);
