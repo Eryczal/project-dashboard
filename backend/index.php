90cfec6e80da0e0a68f457c608f3c4852b6b37df
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-04-18 22:31:54 +0200

    b: Add PHP routing

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,20 @@
 <?php
     require_once "db/db.php";
+    require_once "data.php";
+
+    header("Access-Control-Allow-Origin: $domain");
+
+    $request = $_SERVER["REQUEST_URI"];
+
+    $req = str_replace($baseurl, "", $request);
+
+    switch($req) {
+        case "/register":
+            require __DIR__ . "/api/register.php";
+            break;
+
+        default:
+            http_response_code(404);
+            break;
+    }
 ?>
