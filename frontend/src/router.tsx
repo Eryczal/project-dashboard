360c131df430392571d7f52b3ac5ad7c92d053f9
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-04-18 20:10:50 +0200

    f: Store router basename in env file
    The router’s basename now references the .env file for different production settings.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import TaskPage from "./pages/TaskPage";
 import ProjectPage from "./pages/ProjectPage";
+import AuthPage from "./pages/AuthPage";
 
 export const router = createBrowserRouter(
     [
@@ -12,8 +13,16 @@ export const router = createBrowserRouter(
             path: "/project/:id",
             element: <TaskPage />,
         },
+        {
+            path: "/register",
+            element: <AuthPage routerPage="register" />,
+        },
+        {
+            path: "/login",
+            element: <AuthPage routerPage="login" />,
+        },
     ],
     {
-        basename: "/Strony/project-dashboard/frontend",
+        basename: import.meta.env.VITE_BASENAME,
     }
 );
