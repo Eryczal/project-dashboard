7a6a145e0fbc225e04ecd009820dcc426efa7246
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-04-28 14:32:47 +0200

    f: Display projects from db

--- frontend/src/pages/ProjectPage.tsx
+++ frontend/src/pages/ProjectPage.tsx
@@ -1,22 +1,46 @@
 import { useEffect, useState } from "react";
-import { getUserProjects } from "../data/project";
 import { Project } from "../data/project";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
 
 function ProjectPage() {
+    const { user } = useUser();
     const [projects, setProjects] = useState<Project[] | null>(null);
+    const [redirect, setRedirect] = useState<boolean>(false);
 
     useEffect(() => {
-        let data = getUserProjects();
-        setProjects(data);
+        const getProjects = async (): Promise<void> => {
+            let data = await getUserProjects();
+
+            if (data && "message" in data) {
+                setRedirect(true);
+                return;
+            }
+
+            setProjects(data ? data.projects : null);
+        };
+
+        getProjects().catch(console.error);
     }, []);
 
+    const addProject = async () => {
+    };
+
+    if (redirect) {
+        return <Navigate to="/login" />;
+    }
+
     if (projects === null) {
-        return <main>Wystąpił problem</main>;
+        return (
+            <main>
+                Obecnie nie masz żadnych projektów. <button onClick={addProject}>Stwórz projekt</button>
+            </main>
+        );
     }
 
     return (
         <main>
+            <h1>Witaj {user ? user.name : "anonim"}</h1>
             {projects.map((project) => (
                 <div key={project.id}>
                     <h2>{project.title}</h2>
