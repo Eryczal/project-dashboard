fd1c02c583f5d1865b04b5e722878b8d409b517d
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-05-30 13:04:29 +0200

    f: Add Buttons to Columns

--- frontend/src/components/columns/TaskColumn.tsx
+++ frontend/src/components/columns/TaskColumn.tsx
@@ -1,7 +1,19 @@
+import "./TaskColumn.css";
 import { Column } from "../../types";
+import { MdAddCircleOutline, MdMoreVert } from "react-icons/md";
 
 function TaskColumn({ column }: { column: Column }) {
-    return <>{column.title}</>;
+    return (
+        <div className="column">
+            <header className="column-header">
+                <h2>{column.title}</h2>
+                <div className="column-buttons">
+                    <MdAddCircleOutline />
+                    <MdMoreVert />
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default TaskColumn;
