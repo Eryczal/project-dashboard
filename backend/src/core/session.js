2602caa21181475ced919091345b68b8df3b4937
Eryczal <79263592+Eryczal@users.noreply.github.com>
2025-05-08 21:07:40 +0200

    b: Add sessions

--- backend/src/core/session.js
+++ backend/src/core/session.js
@@ -1,5 +1,7 @@
-import session from "express-session";
+import crypto from "crypto";
 import dotenv from "dotenv";
+import session from "express-session";
+import logger from "./logger.js";
 
 dotenv.config();
 
@@ -7,11 +9,67 @@ export const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    rolling: true,
     cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        maxAge: 1000 * 60 * 30,
+        maxAge: 1000 * 60 * 60,
     },
 });
+
+export function regenerateSession(req, userId, reload = false) {
+    if (!req.session.nonce || reload) {
+        req.session.nonce = crypto.randomBytes(32).toString("hex");
+    }
+
+    if (!req.session.ipAddress || reload) {
+        req.session.ipAddress = req.ip;
+    }
+
+    if (!req.session.userAgent || reload) {
+        req.session.userAgent = req.get("User-Agent");
+    }
+
+    req.session.userId = userId;
+    req.session.OBSOLETE = true;
+    req.session.EXPIRES = Date.now() + 60 * 1000;
+
+    req.session.regenerate((e) => {
+        if (e) {
+            logger.error(`Session regeneration failed: ${e}`);
+        } else {
+            req.session.userId = userId;
+            req.session.OBSOLETE = false;
+            req.session.EXPIRES = null;
+        }
+    });
+}
+
+export function checkSession(req) {
+    try {
+        if (req.session.OBSOLETE && req.session.EXPIRES && req.session.EXPIRES < Date.now()) {
+            throw new Error("Attempt to use expired session");
+        }
+
+        if (typeof req.session.userId !== "number") {
+            throw new Error("No session started");
+        }
+
+        if (req.session.ipAddress !== req.ip) {
+            throw new Error("IP address mismatch");
+        }
+
+        if (req.session.userAgent !== req.get("User-Agent")) {
+            throw new Error("User-Agent mismatch");
+        }
+
+        if (!req.session.OBSOLETE && Math.floor(Math.random() * 100) === 0) {
+            regenerateSession(req, req.session.userId, true);
+        }
+
+        return true;
+    } catch (e) {
+        logger.error(`Session check failed: ${e}`);
+        return false;
+    }
+}
--- backend/src/modules/common/common-validations.js
+++ backend/src/modules/common/common-validations.js
@@ -1,8 +1,13 @@
+import { checkSession } from "../../core/session.js";
+
 export default {
     string: (req, res, next, value) => {
         return typeof value === "string";
     },
     number: (req, res, next, value) => {
         return !isNaN(value) && !isNaN(parseFloat(value));
+    },
+    sessionRequired: (req, res, next) => {
+        return checkSession(req);
     },
 };
