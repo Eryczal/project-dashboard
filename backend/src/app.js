08d34a7b1c88fe8accb95f505c1639cbee1829f0
Eryczal <79263592+Eryczal@users.noreply.github.com>
2025-05-09 15:04:43 +0200

    b: Allow cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,22 @@ import { initRoutes } from "./core/router.js";
 import { initValidations } from "./core/validator.js";
 import logger from "./core/logger.js";
 import mysql from "./core/mysql.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessionMiddleware);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    next();
+});
 const PORT = 3010;
 
 await initValidations();
